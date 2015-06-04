8e1e65585315b609de7f4466fb39e709298015da
Manuel Martinez Peinado <kketzal@gmail.com>
2015-06-04 20:33:41 +0200

    R script for read the DATA

--- read_data.R
+++ read_data.R
@@ -1,28 +1,13 @@
+################################################################
+## Script for read the data. 
+## This script is imported in all others scripts:
+##      plot1.R, plot2.R, plot3.R and plot4.R
+## 
+################################################################
+
 suppressPackageStartupMessages(library(data.table))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(lubridate))
-
-########################################################################################
-# this example is extracted from StackOverflow:
-# Create a new character class "MyDate". this class is a function that read date 
-# with the format d/m/Y and convert it into R Date. 
-#
-# DOESN'T WORK WITH "data.table" package.
-#
-setClass('myDate') 
-setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
-
-# Similar to Time:
-# setClass("myTime")
-# setAs("character","myTime", function(from) hms(from) )
-#
-#x <- read.csv.sql("household_power_consumption.txt", 
-#                  sep = ";", 
-#                  nrows = 10,
-#                  colClasses = c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), 
-#                  sql = "SELECT * FROM file WHERE Date BETWEEN date('2007-02-01') AND date('2007-02-02')")
-########################################################################################
-
 
 ## Check if data file exists, if not, STOP the script execution.
 if(!file.exists("household_power_consumption.txt")){
@@ -50,4 +35,29 @@ clean_data <- clean_data %>%
               filter(Date >= "2007-02-01" & Date <= "2007-02-02" ) %>%  # filter 2 days only
               mutate_each(funs(as.numeric), -c(Date,Time)) %>%          # to mumeric all, less Date & Time
               mutate(DateTime = ymd_hms(paste(Date,Time)))              # Create a new column with Date & Time together
-             
+
+
+
+
+## THIS SECTION IS FOR MY OWN KNOWLEDGE
+########################################################################################
+# this example is extracted from StackOverflow:
+# Create a new character class "MyDate". this class is a function that read date 
+# with the format d/m/Y and convert it into R Date. 
+#
+# DOESN'T WORK WITH "data.table" package.
+#
+# setClass('myDate') 
+# setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
+
+# Similar to Time:
+# setClass("myTime")
+# setAs("character","myTime", function(from) hms(from) )
+#
+#x <- read.csv.sql("household_power_consumption.txt", 
+#                  sep = ";", 
+#                  nrows = 10,
+#                  colClasses = c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), 
+#                  sql = "SELECT * FROM file WHERE Date BETWEEN date('2007-02-01') AND date('2007-02-02')")
+########################################################################################
+
