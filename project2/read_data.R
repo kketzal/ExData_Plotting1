ec94f759152f4012006f4d5265d9dd0fde34e641
Manuel Martinez Peinado <kketzal@gmail.com>
2015-06-07 22:03:58 +0200

    project 2 folder

---
+++ project2/read_data.R
@@ -0,0 +1,18 @@
+
+# assign file names to vars... 
+data_pm25_file <- "summarySCC_PM25.rds"
+data_source_classif_file <- "Source_Classification_Code.rds"
+
+# check if files exist...
+if(!match(data_pm25_file, dir(), nomatch = FALSE))
+    stop(paste("The file", data_pm25_file, "doesn't exist!"))
+
+if(!match(data_source_classif_file, dir(), nomatch = FALSE))
+    stop(paste("The file", data_source_classif_file, "doesn't exist!"))
+
+# read the data
+message(paste("Reading", data_pm25_file, "...", collapse = ""))
+data_pm25 <- readRDS(data_pm25_file)
+message(paste("Reading", data_source_classif_file, "..."))
+data_source_classif <- readRDS(data_source_classif_file)
+message("Done!")
