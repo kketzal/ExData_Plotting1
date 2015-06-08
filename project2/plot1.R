10c5c94db6f629d4b3e5689d0355da64aaea53ed
Manuel Martinez Peinado <kketzal@gmail.com>
2015-06-08 20:27:07 +0200

    Question 1 - R file

---
+++ project2/plot1.R
@@ -0,0 +1,18 @@
+# Read the data from external script
+source("read_data.R")
+
+# load library "dplyr" for "group" and "summarise" purpouses
+library(dplyr)
+
+# group the dataframe (NEI )by year, and sum total emissions from all sources
+grouped_by_year <- summarise(group_by(NEI, year), Total_Emissions = sum(Emissions))
+
+# Plot the result
+png("plot1.png")
+
+plot(grouped_by_year$year, grouped_by_year$Total_Emissions,      
+     xlab = "Year",
+     ylab = "Total Emissions (tons)",
+     type = "l")
+
+dev.off()
