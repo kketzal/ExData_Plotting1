a10899bd2383779634dafbe4da5e5ab9c8f9f6fa
Manuel Martinez Peinado <kketzal@gmail.com>
2015-06-17 20:30:52 +0200

    plot 6 R script

--- project2/plot6.R
+++ project2/plot6.R
@@ -45,23 +45,34 @@ all_data <- merge(BALTIMORE_L.A._DATA, MOTOR_VEHICLES_DATA)
 # group the dataframe (baltimore_df) by year, and type
 grouped <- group_by(all_data, year, fips)
 
-# Summarise grouped dataframe and compute the MEAN of emissions 
-my_summarize <- summarise(grouped, Total_Emissions = mean(Emissions))
+# Summarise grouped dataframe and compute the SUM of emissions 
+my_summarize <- summarise(grouped, Total_Emissions = sum(Emissions))
 
 # the my_summarize dataset appears not be sorted by year....we need to order it.
 my_summarize <- my_summarize[order(fips, year)]
 
+# if we want to calculate PERCENTAGE CHANGE on every city, subset the data for each city
+# and calculate the percentage change on each dataset
+my_summarize_Baltimore <- my_summarize[1:4]
+my_summarize_Los_Angeles <- my_summarize[5:8]
+
+# add a new variable "since1999" with the percentage change
+my_summarize_Baltimore <- transform(my_summarize_Baltimore, since1999 = (Total_Emissions-Total_Emissions[1])/Total_Emissions[1]*100)
+my_summarize_Los_Angeles <- transform(my_summarize_Los_Angeles, since1999 = (Total_Emissions-Total_Emissions[1])/Total_Emissions[1]*100)
+
+# BIND the 2 previous datasets in one dataset for plotting
+my_summarize <- rbind(my_summarize_Baltimore, my_summarize_Los_Angeles)
 
 # Plotting, with assign explicitly the image width
 png("plot6.png", width = 650, height = 360)
 
 # qplot() doesn't print anything from R files. you need to assign the result
 # to a variable ('p' in this case), and then, explicitly call to print() function
-p <- qplot(year, Total_Emissions, data = my_summarize, 
+p <- qplot(year, since1999, data = my_summarize, 
            facets =.~fips, 
            geom = "line",
            xlab= "Year",
-           ylab = "MEAN of Total Emissions (tons)",
+           ylab = "% CHANGE of Total Emissions",
            main = "Greater Changes Over Time in Motor Vehicle Emissions")
 
 print(p)
